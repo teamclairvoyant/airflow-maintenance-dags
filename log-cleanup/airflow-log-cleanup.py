15571f0364e88665e47e78622415fa810603c1bb
rssanders3 <rssanders3@gmail.com>
2017-05-22 15:18:33 -0700

    Allowing the user to define the number of workers for the log clean up so that way logs from all the work nodes are cleared out

--- log-cleanup/airflow-log-cleanup.py
+++ log-cleanup/airflow-log-cleanup.py
@@ -17,11 +17,12 @@ airflow trigger_dag --conf '{"maxLogAgeInDays":30}' airflow-log-cleanup
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-log-cleanup
 START_DATE = datetime.now() - timedelta(minutes=1)
 BASE_LOG_FOLDER = conf.get("core", "BASE_LOG_FOLDER")
-SCHEDULE_INTERVAL = "@daily"        # How often to Run. @daily - Once a day at Midnight
+SCHEDULE_INTERVAL = None        # How often to Run. @daily - Once a day at Midnight
 DAG_OWNER_NAME = "operations"       # Who is listed as the owner of this DAG in the Airflow Web Server
 ALERT_EMAIL_ADDRESSES = []          # List of email address to send email alerts to if this job fails
-DEFAULT_MAX_LOG_AGE_IN_DAYS = 30    # Length to retain the log files if not already provided in the conf. If this is set to 30, the job will remove those files that are 30 days old or odler.
-ENABLE_DELETE = True                # Whether the job should delete the logs or not. Included if you want to temporarily avoid deleting the logs.
+DEFAULT_MAX_LOG_AGE_IN_DAYS = 30    # Length to retain the log files if not already provided in the conf. If this is set to 30, the job will remove those files that are 30 days old or odler
+ENABLE_DELETE = True                # Whether the job should delete the logs or not. Included if you want to temporarily avoid deleting the logs
+NUMBER_OF_WORKERS = 1               # The number of worker nodes you have in Airflow. Will attempt to run this process for however many workers there are so that each worker gets its logs cleared.
 
 default_args = {
     'owner': DAG_OWNER_NAME,
@@ -78,8 +79,10 @@ fi
 echo "Finished Running Cleanup Process"
 """
 
-log_cleanup = BashOperator(
-    task_id='log_cleanup',
-    bash_command=log_cleanup,
-    provide_context=True,
-    dag=dag)
+for log_cleanup_id in range(1, NUMBER_OF_WORKERS + 1):
+
+    log_cleanup = BashOperator(
+        task_id='log_cleanup_' + str(log_cleanup_id),
+        bash_command=log_cleanup,
+        provide_context=True,
+        dag=dag)
