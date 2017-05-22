9f04acdaf8b1020b293f9f16a19c249e053bc839
rssanders3 <rssanders3@gmail.com>
2017-05-22 13:05:13 -0700

    Re-setting the SCHEDULE_INTERVAL in airflow-db-cleanup

--- db-cleanup/airflow-db-cleanup.py
+++ db-cleanup/airflow-db-cleanup.py
@@ -16,7 +16,7 @@ airflow trigger_dag --conf '{"maxDBEntryAgeInDays":30}' airflow-db-cleanup
 
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-db-cleanup
 START_DATE = datetime.now() - timedelta(minutes=1)
-SCHEDULE_INTERVAL = None            # How often to Run. @daily - Once a day at Midnight
+SCHEDULE_INTERVAL = "@daily"            # How often to Run. @daily - Once a day at Midnight (UTC)
 DAG_OWNER_NAME = "operations"           # Who is listed as the owner of this DAG in the Airflow Web Server
 ALERT_EMAIL_ADDRESSES = []              # List of email address to send email alerts to if this job fails
 DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS = 30   # Length to retain the log files if not already provided in the conf. If this is set to 30, the job will remove those files that are 30 days old or older.
