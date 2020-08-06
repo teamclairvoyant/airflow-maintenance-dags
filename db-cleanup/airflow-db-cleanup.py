teamclairvoyant/airflow-maintenance-dags

commit a5fc73bf1ef04be1bc73aa55ea5a69b5232c0bf8

flake8 compliance

db-cleanup/airflow-db-cleanup.py
@@ -1,5 +1,7 @@
 """
-A maintenance workflow that you can deploy into Airflow to periodically clean out the DagRun, TaskInstance, Log, XCom, Job DB and SlaMiss entries to avoid having too much data in your Airflow MetaStore.
+A maintenance workflow that you can deploy into Airflow to periodically clean
+out the DagRun, TaskInstance, Log, XCom, Job DB and SlaMiss entries to avoid
+having too much data in your Airflow MetaStore.
 
 airflow trigger_dag --conf '{"maxDBEntryAgeInDays":30}' airflow-db-cleanup
 
@@ -7,7 +9,8 @@ airflow trigger_dag --conf '{"maxDBEntryAgeInDays":30}' airflow-db-cleanup
     maxDBEntryAgeInDays:<INT> - Optional
 
 """
-from airflow.models import DAG, DagRun, TaskInstance, Log, XCom, SlaMiss, DagModel, Variable
+from airflow.models import DAG, DagRun, TaskInstance, Log, XCom, SlaMiss, \
+    DagModel, Variable
 from airflow.jobs import BaseJob
 from airflow import settings
 from airflow.operators.python_operator import PythonOperator
@@ -20,26 +23,81 @@ import dateutil.parser
 import airflow
 
 try:
-    from airflow.utils import timezone  # airflow.utils.timezone is available from v1.10 onwards
+    # airflow.utils.timezone is available from v1.10 onwards
+    from airflow.utils import timezone
     now = timezone.utcnow
 except ImportError:
     now = datetime.utcnow
 
-DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-db-cleanup
+# airflow-db-cleanup
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 START_DATE = airflow.utils.dates.days_ago(1)
-SCHEDULE_INTERVAL = "@daily"            # How often to Run. @daily - Once a day at Midnight (UTC)
-DAG_OWNER_NAME = "operations"           # Who is listed as the owner of this DAG in the Airflow Web Server
-ALERT_EMAIL_ADDRESSES = []              # List of email address to send email alerts to if this job fails
-DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS = int(Variable.get("airflow_db_cleanup__max_db_entry_age_in_days", 30)) # Length to retain the log files if not already provided in the conf. If this is set to 30, the job will remove those files that are 30 days old or older.
-ENABLE_DELETE = True                    # Whether the job should delete the db entries or not. Included if you want to temporarily avoid deleting the db entries.
-DATABASE_OBJECTS = [                    # List of all the objects that will be deleted. Comment out the DB objects you want to skip.
-    {"airflow_db_model": DagRun, "age_check_column": DagRun.execution_date, "keep_last": True, "keep_last_filters": [DagRun.external_trigger==False], "keep_last_group_by": DagRun.dag_id},
-    {"airflow_db_model": TaskInstance, "age_check_column": TaskInstance.execution_date, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
-    {"airflow_db_model": Log, "age_check_column": Log.dttm, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
-    {"airflow_db_model": XCom, "age_check_column": XCom.execution_date, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
-    {"airflow_db_model": BaseJob, "age_check_column": BaseJob.latest_heartbeat, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
-    {"airflow_db_model": SlaMiss, "age_check_column": SlaMiss.execution_date, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
-    {"airflow_db_model": DagModel, "age_check_column": DagModel.last_scheduler_run, "keep_last": False, "keep_last_filters": None, "keep_last_group_by": None},
+# How often to Run. @daily - Once a day at Midnight (UTC)
+SCHEDULE_INTERVAL = "@daily"
+# Who is listed as the owner of this DAG in the Airflow Web Server
+DAG_OWNER_NAME = "operations"
+# List of email address to send email alerts to if this job fails
+ALERT_EMAIL_ADDRESSES = []
+# Length to retain the log files if not already provided in the conf. If this
+# is set to 30, the job will remove those files that arE 30 days old or older.
+
+DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS = int(
+    Variable.get("airflow_db_cleanup__max_db_entry_age_in_days", 30)
+)
+# Whether the job should delete the db entries or not. Included if you want to
+# temporarily avoid deleting the db entries.
+ENABLE_DELETE = True
+# List of all the objects that will be deleted. Comment out the DB objects you
+# want to skip.
+DATABASE_OBJECTS = [
+    {
+        "airflow_db_model": DagRun,
+        "age_check_column": DagRun.execution_date,
+        "keep_last": True,
+        "keep_last_filters": [DagRun.external_trigger is False],
+        "keep_last_group_by": DagRun.dag_id},
+    {
+        "airflow_db_model": TaskInstance,
+        "age_check_column": TaskInstance.execution_date,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
+    {
+        "airflow_db_model": Log,
+        "age_check_column": Log.dttm,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
+    {
+        "airflow_db_model": XCom,
+        "age_check_column": XCom.execution_date,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
+    {
+        "airflow_db_model": BaseJob,
+        "age_check_column": BaseJob.latest_heartbeat,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
+    {
+        "airflow_db_model": SlaMiss,
+        "age_check_column": SlaMiss.execution_date,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
+    {
+        "airflow_db_model": DagModel,
+        "age_check_column": DagModel.last_scheduler_run,
+        "keep_last": False,
+        "keep_last_filters": None,
+        "keep_last_group_by": None
+    },
 ]
 
 session = settings.Session()
@@ -55,7 +113,12 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(DAG_ID, default_args=default_args, schedule_interval=SCHEDULE_INTERVAL, start_date=START_DATE)
+dag = DAG(
+    DAG_ID,
+    default_args=default_args,
+    schedule_interval=SCHEDULE_INTERVAL,
+    start_date=START_DATE
+)
 if hasattr(dag, 'doc_md'):
     dag.doc_md = __doc__
 if hasattr(dag, 'catchup'):
@@ -68,10 +131,16 @@ def print_configuration_function(**context):
     logging.info("dag_run.conf: " + str(dag_run_conf))
     max_db_entry_age_in_days = None
     if dag_run_conf:
-        max_db_entry_age_in_days = dag_run_conf.get("maxDBEntryAgeInDays", None)
+        max_db_entry_age_in_days = dag_run_conf.get(
+            "maxDBEntryAgeInDays", None
+        )
     logging.info("maxDBEntryAgeInDays from dag_run.conf: " + str(dag_run_conf))
     if (max_db_entry_age_in_days is None or max_db_entry_age_in_days < 1):
-        logging.info("maxDBEntryAgeInDays conf variable isn't included or Variable value is less than 1. Using Default '" + str(DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS) + "'")
+        logging.info(
+            "maxDBEntryAgeInDays conf variable isn't included or Variable " +
+            "value is less than 1. Using Default '" +
+            str(DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS) + "'"
+        )
         max_db_entry_age_in_days = DEFAULT_MAX_DB_ENTRY_AGE_IN_DAYS
     max_date = now() + timedelta(-max_db_entry_age_in_days)
     logging.info("Finished Loading Configurations")
@@ -87,6 +156,7 @@ def print_configuration_function(**context):
     logging.info("Setting max_execution_date to XCom for Downstream Processes")
     context["ti"].xcom_push(key="max_date", value=max_date.isoformat())
 
+
 print_configuration = PythonOperator(
     task_id='print_configuration',
     python_callable=print_configuration_function,
@@ -97,7 +167,9 @@ print_configuration = PythonOperator(
 def cleanup_function(**context):
 
     logging.info("Retrieving max_execution_date from XCom")
-    max_date = context["ti"].xcom_pull(task_ids=print_configuration.task_id, key="max_date")
+    max_date = context["ti"].xcom_pull(
+        task_ids=print_configuration.task_id, key="max_date"
+    )
     max_date = dateutil.parser.parse(max_date)  # stored as iso8601 str in xcom
 
     airflow_db_model = context["params"].get("airflow_db_model")
@@ -122,9 +194,11 @@ def cleanup_function(**context):
 
     logging.info("Running Cleanup Process...")
 
-    query = session.query(airflow_db_model).options(load_only(age_check_column))
-    
-    logging.info("INITIAL QUERY : " +  str(query))
+    query = session.query(airflow_db_model).options(
+        load_only(age_check_column)
+    )
+
+    logging.info("INITIAL QUERY : " + str(query))
 
     if keep_last:
 
@@ -134,13 +208,12 @@ def cleanup_function(**context):
         if keep_last_filters is not None:
             for entry in keep_last_filters:
                 subquery = subquery.filter(entry)
-                
-            logging.info("SUB QUERY [keep_last_filters]: " +  str(subquery))
 
-        if  keep_last_group_by is not None:
+            logging.info("SUB QUERY [keep_last_filters]: " + str(subquery))
+
+        if keep_last_group_by is not None:
             subquery = subquery.group_by(keep_last_group_by)
-            logging.info("SUB QUERY [keep_last_group_by]: " +  str(subquery))
-            
+            logging.info("SUB QUERY [keep_last_group_by]: " + str(subquery))
 
         subquery = subquery.from_self()
 
@@ -148,22 +221,31 @@ def cleanup_function(**context):
             and_(age_check_column.notin_(subquery)),
             and_(age_check_column <= max_date)
         )
-        
+
     else:
         query = query.filter(age_check_column <= max_date,)
 
     entries_to_delete = query.all()
 
     logging.info("Query: " + str(query))
-    logging.info("Process will be Deleting the following " + str(airflow_db_model.__name__) + "(s):")
+    logging.info(
+        "Process will be Deleting the following " +
+        str(airflow_db_model.__name__) + "(s):"
+    )
     for entry in entries_to_delete:
-        logging.info("\tEntry: " + str(entry) + ", Date: " + str(entry.__dict__[str(age_check_column).split(".")[1]]))
+        logging.info(
+            "\tEntry: " + str(entry) + ", Date: " +
+            str(entry.__dict__[str(age_check_column).split(".")[1]])
+        )
 
-    logging.info("Process will be Deleting " + str(len(entries_to_delete)) + " " + str(airflow_db_model.__name__) + "(s)")
+    logging.info(
+        "Process will be Deleting " + str(len(entries_to_delete)) + " " +
+        str(airflow_db_model.__name__) + "(s)"
+    )
 
     if ENABLE_DELETE:
         logging.info("Performing Delete...")
-        #using bulk delete
+        # using bulk delete
         query.delete(synchronize_session=False)
         session.commit()
         logging.info("Finished Performing Delete")
@@ -171,6 +253,7 @@ def cleanup_function(**context):
         logging.warn("You're opted to skip deleting the db entries!!!")
 
     logging.info("Finished Running Cleanup Process")
+
 
 for db_object in DATABASE_OBJECTS:
 

backup-configs/airflow-backup-configs.py
@@ -1,5 +1,6 @@
 """
-A maintenance workflow that you can deploy into Airflow to periodically take backups of various Airflow configurations and files.
+A maintenance workflow that you can deploy into Airflow to periodically take
+backups of various Airflow configurations and files.
 
 airflow trigger_dag airflow-backup-configs
 
@@ -12,14 +13,17 @@ import os
 import airflow
 import logging
 import subprocess
-
-DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-backup-configs
+# airflow-backup-configs
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
+# How often to Run. @daily - Once a day at Midnight
 START_DATE = airflow.utils.dates.days_ago(1)
-SCHEDULE_INTERVAL = "@daily"        # How often to Run. @daily - Once a day at Midnight
-DAG_OWNER_NAME = "operations"       # Who is listed as the owner of this DAG in the Airflow Web Server
-ALERT_EMAIL_ADDRESSES = []          # List of email address to send email alerts to if this job fails
-
-BACKUP_FOLDER_DATE_FORMAT = "%Y%m%d%H%M%S"  # Format options: https://www.tutorialspoint.com/python/time_strftime.htm
+# Who is listed as the owner of this DAG in the Airflow Web Server
+SCHEDULE_INTERVAL = "@daily"
+# List of email address to send email alerts to if this job fails
+DAG_OWNER_NAME = "operations"
+ALERT_EMAIL_ADDRESSES = []
+# Format options: https://www.tutorialspoint.com/python/time_strftime.htm
+BACKUP_FOLDER_DATE_FORMAT = "%Y%m%d%H%M%S"
 BACKUP_HOME_DIRECTORY = "/tmp/airflow_backups"
 BACKUPS_ENABLED = {
     "dag_directory": True,
@@ -27,7 +31,8 @@ BACKUPS_ENABLED = {
     "airflow_cfg": True,
     "pip_packages": True
 }
-BACKUP_RETENTION_COUNT = 7          # How many backups to retain (not including the one that was just taken)
+# How many backups to retain (not including the one that was just taken)
+BACKUP_RETENTION_COUNT = 7
 
 default_args = {
     'owner': DAG_OWNER_NAME,
@@ -39,7 +44,12 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(DAG_ID, default_args=default_args, schedule_interval=SCHEDULE_INTERVAL, start_date=START_DATE)
+dag = DAG(
+    DAG_ID,
+    default_args=default_args,
+    schedule_interval=SCHEDULE_INTERVAL,
+    start_date=START_DATE
+)
 if hasattr(dag, 'doc_md'):
     dag.doc_md = __doc__
 if hasattr(dag, 'catchup'):
@@ -54,17 +64,31 @@ def print_configuration_fn(**context):
     BACKUP_DIRECTORY = BACKUP_HOME_DIRECTORY + "/" + BACKUP_FOLDER_DATE + "/"
 
     logging.info("Configurations:")
-    logging.info("BACKUP_FOLDER_DATE_FORMAT:    " + str(BACKUP_FOLDER_DATE_FORMAT))
+    logging.info(
+        "BACKUP_FOLDER_DATE_FORMAT:    " + str(BACKUP_FOLDER_DATE_FORMAT)
+    )
     logging.info("BACKUP_FOLDER_DATE:           " + str(BACKUP_FOLDER_DATE))
     logging.info("BACKUP_HOME_DIRECTORY:        " + str(BACKUP_HOME_DIRECTORY))
     logging.info("BACKUP_DIRECTORY:             " + str(BACKUP_DIRECTORY))
-    logging.info("BACKUP_RETENTION_COUNT:       " + str(BACKUP_RETENTION_COUNT))
+    logging.info(
+        "BACKUP_RETENTION_COUNT:       " + str(BACKUP_RETENTION_COUNT)
+    )
     logging.info("")
 
     logging.info("Pushing to XCom for Downstream Processes")
-    context["ti"].xcom_push(key="backup_configs.backup_home_directory", value=BACKUP_HOME_DIRECTORY)
-    context["ti"].xcom_push(key="backup_configs.backup_directory", value=BACKUP_DIRECTORY)
-    context["ti"].xcom_push(key="backup_configs.backup_retention_count", value=BACKUP_RETENTION_COUNT)
+    context["ti"].xcom_push(
+        key="backup_configs.backup_home_directory",
+        value=BACKUP_HOME_DIRECTORY
+    )
+    context["ti"].xcom_push(
+        key="backup_configs.backup_directory",
+        value=BACKUP_DIRECTORY
+    )
+    context["ti"].xcom_push(
+        key="backup_configs.backup_retention_count",
+        value=BACKUP_RETENTION_COUNT
+    )
+
 
 print_configuration_op = PythonOperator(
     task_id='print_configuration',
@@ -78,7 +102,7 @@ def execute_shell_cmd(cmd):
     proc = subprocess.Popen(cmd, shell=True, universal_newlines=True)
     proc.communicate()
     exit_code = proc.returncode
-    if exit_code is not 0:
+    if exit_code != 0:
         exit(exit_code)
 
 
@@ -86,7 +110,10 @@ def create_backup_directory_fn(**context):
     logging.info("Executing create_backup_directory_fn")
 
     logging.info("Loading Configurations...")
-    BACKUP_DIRECTORY = context["ti"].xcom_pull(task_ids=print_configuration_op.task_id, key='backup_configs.backup_directory')
+    BACKUP_DIRECTORY = context["ti"].xcom_pull(
+        task_ids=print_configuration_op.task_id,
+        key='backup_configs.backup_directory'
+    )
 
     logging.info("Configurations:")
     logging.info("BACKUP_DIRECTORY:    " + str(BACKUP_DIRECTORY))
@@ -106,8 +133,14 @@ def delete_old_backups_fn(**context):
     logging.info("Executing delete_old_backups_fn")
 
     logging.info("Loading Configurations...")
-    BACKUP_HOME_DIRECTORY = context["ti"].xcom_pull(task_ids=print_configuration_op.task_id, key='backup_configs.backup_home_directory')
-    BACKUP_RETENTION_COUNT = context["ti"].xcom_pull(task_ids=print_configuration_op.task_id, key='backup_configs.backup_retention_count')
+    BACKUP_HOME_DIRECTORY = context["ti"].xcom_pull(
+        task_ids=print_configuration_op.task_id,
+        key='backup_configs.backup_home_directory'
+    )
+    BACKUP_RETENTION_COUNT = context["ti"].xcom_pull(
+        task_ids=print_configuration_op.task_id,
+        key='backup_configs.backup_retention_count'
+    )
 
     logging.info("Configurations:")
     logging.info("BACKUP_HOME_DIRECTORY:    " + str(BACKUP_HOME_DIRECTORY))
@@ -115,21 +148,31 @@ def delete_old_backups_fn(**context):
     logging.info("")
 
     if BACKUP_RETENTION_COUNT < 0:
-        logging.info("Retention is less then 0. Assuming to allow infinite backups. Skipping...")
+        logging.info(
+            "Retention is less then 0. Assuming to allow infinite backups. "
+            "Skipping..."
+        )
         return
 
-    backup_folders = [os.path.join(BACKUP_HOME_DIRECTORY, f) for f in os.listdir(BACKUP_HOME_DIRECTORY) if os.path.isdir(os.path.join(BACKUP_HOME_DIRECTORY, f))]
+    backup_folders = [
+        os.path.join(BACKUP_HOME_DIRECTORY, f)
+        for f in os.listdir(BACKUP_HOME_DIRECTORY)
+        if os.path.isdir(os.path.join(BACKUP_HOME_DIRECTORY, f))
+    ]
     backup_folders.reverse()
     logging.info("backup_folders: " + str(backup_folders))
     logging.info("")
 
     cnt = 0
     for backup_folder in backup_folders:
-        logging.info("cnt = " + str(cnt) + ", backup_folder = " + str(backup_folder))
+        logging.info(
+            "cnt = " + str(cnt) + ", backup_folder = " + str(backup_folder)
+        )
         if cnt > BACKUP_RETENTION_COUNT:
             logging.info("Deleting Backup Folder: " + str(backup_folder))
             execute_shell_cmd("rm -rf " + str(backup_folder))
         cnt += 1
+
 
 delete_old_backups_op = PythonOperator(
     task_id='delete_old_backups',
@@ -144,7 +187,10 @@ def general_backup_fn(**context):
     logging.info("Loading Configurations...")
     PATH_TO_BACKUP = context["params"].get("path_to_backup")
     TARGET_DIRECTORY_NAME = context["params"].get("target_directory_name")
-    BACKUP_DIRECTORY = context["ti"].xcom_pull(task_ids=print_configuration_op.task_id, key='backup_configs.backup_directory')
+    BACKUP_DIRECTORY = context["ti"].xcom_pull(
+        task_ids=print_configuration_op.task_id,
+        key='backup_configs.backup_directory'
+    )
 
     logging.info("Configurations:")
     logging.info("PATH_TO_BACKUP:           " + str(PATH_TO_BACKUP))
@@ -152,20 +198,27 @@ def general_backup_fn(**context):
     logging.info("BACKUP_DIRECTORY:         " + str(BACKUP_DIRECTORY))
     logging.info("")
 
-    execute_shell_cmd("cp -r -n " + str(PATH_TO_BACKUP) + " " + str(BACKUP_DIRECTORY) + (TARGET_DIRECTORY_NAME if TARGET_DIRECTORY_NAME is not None else ""))
+    execute_shell_cmd(
+        "cp -r -n " + str(PATH_TO_BACKUP) + " " + str(BACKUP_DIRECTORY) +
+        (TARGET_DIRECTORY_NAME if TARGET_DIRECTORY_NAME is not None else "")
+    )
 
 
 def pip_packages_backup_fn(**context):
     logging.info("Executing pip_packages_backup_fn")
 
     logging.info("Loading Configurations...")
-    BACKUP_DIRECTORY = context["ti"].xcom_pull(task_ids=print_configuration_op.task_id, key='backup_configs.backup_directory')
+    BACKUP_DIRECTORY = context["ti"].xcom_pull(
+        task_ids=print_configuration_op.task_id,
+        key='backup_configs.backup_directory'
+    )
 
     logging.info("Configurations:")
     logging.info("BACKUP_DIRECTORY:     " + str(BACKUP_DIRECTORY))
     logging.info("")
 
     execute_shell_cmd("pip freeze > " + BACKUP_DIRECTORY + "pip_freeze.out")
+
 
 if BACKUPS_ENABLED.get("dag_directory"):
     backup_op = PythonOperator(
@@ -181,7 +234,10 @@ if BACKUPS_ENABLED.get("log_directory"):
     backup_op = PythonOperator(
         task_id='backup_log_directory',
         python_callable=general_backup_fn,
-        params={"path_to_backup": conf.get("core", "BASE_LOG_FOLDER"), "target_directory_name": "logs"},
+        params={
+            "path_to_backup": conf.get("core", "BASE_LOG_FOLDER"),
+            "target_directory_name": "logs"
+        },
         provide_context=True,
         dag=dag)
     create_backup_directory_op.set_downstream(backup_op)
@@ -191,7 +247,9 @@ if BACKUPS_ENABLED.get("airflow_cfg"):
     backup_op = PythonOperator(
         task_id='backup_airflow_cfg',
         python_callable=general_backup_fn,
-        params={"path_to_backup": conf.get("core", "AIRFLOW_HOME") + "/airflow.cfg"},
+        params={
+            "path_to_backup": conf.get("core", "AIRFLOW_HOME") + "/airflow.cfg"
+        },
         provide_context=True,
         dag=dag)
     create_backup_directory_op.set_downstream(backup_op)

clear-missing-dags/airflow-clear-missing-dags.py
@@ -15,12 +15,18 @@ import logging
 import airflow
 
 
-DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-clear-missing-dags
+# airflow-clear-missing-dags
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 START_DATE = airflow.utils.dates.days_ago(1)
-SCHEDULE_INTERVAL = "@daily"        # How often to Run. @daily - Once a day at Midnight
-DAG_OWNER_NAME = "operations"       # Who is listed as the owner of this DAG in the Airflow Web Server
-ALERT_EMAIL_ADDRESSES = []          # List of email address to send email alerts to if this job fails
-ENABLE_DELETE = True                # Whether the job should delete the logs or not. Included if you want to temporarily avoid deleting the logs
+# How often to Run. @daily - Once a day at Midnight
+SCHEDULE_INTERVAL = "@daily"
+# Who is listed as the owner of this DAG in the Airflow Web Server
+DAG_OWNER_NAME = "operations"
+# List of email address to send email alerts to if this job fails
+ALERT_EMAIL_ADDRESSES = []
+# Whether the job should delete the logs or not. Included if you want to
+# temporarily avoid deleting the logs
+ENABLE_DELETE = True
 
 default_args = {
     'owner': DAG_OWNER_NAME,
@@ -33,7 +39,12 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(DAG_ID, default_args=default_args, schedule_interval=SCHEDULE_INTERVAL, start_date=START_DATE)
+dag = DAG(
+    DAG_ID,
+    default_args=default_args,
+    schedule_interval=SCHEDULE_INTERVAL,
+    start_date=START_DATE
+)
 if hasattr(dag, 'doc_md'):
     dag.doc_md = __doc__
 if hasattr(dag, 'catchup'):
@@ -70,18 +81,30 @@ def clear_missing_dags_fn(**context):
             fileloc = dag.fileloc
 
         if fileloc is None:
-            logging.info("After checking DAG '" + str(dag) + "', the fileloc was set to None so assuming the Python definition file DOES NOT exist")
+            logging.info(
+                "After checking DAG '" + str(dag) +
+                "', the fileloc was set to None so assuming the Python " +
+                "definition file DOES NOT exist"
+            )
             entries_to_delete.append(dag)
         elif not os.path.exists(fileloc):
-            logging.info("After checking DAG '" + str(dag) + "', the Python definition file DOES NOT exist: " + fileloc)
+            logging.info(
+                "After checking DAG '" + str(dag) +
+                "', the Python definition file DOES NOT exist: " + fileloc
+            )
             entries_to_delete.append(dag)
         else:
-            logging.info("After checking DAG '" + str(dag) + "', the Python definition file does exist: " + fileloc)
+            logging.info(
+                "After checking DAG '" + str(dag) +
+                "', the Python definition file does exist: " + fileloc
+            )
 
     logging.info("Process will be Deleting the DAG(s) from the DB:")
     for entry in entries_to_delete:
         logging.info("\tEntry: " + str(entry))
-    logging.info("Process will be Deleting " + str(len(entries_to_delete)) + " DAG(s)")
+    logging.info(
+        "Process will be Deleting " + str(len(entries_to_delete)) + " DAG(s)"
+    )
 
     if ENABLE_DELETE:
         logging.info("Performing Delete...")

delete-broken-dags/airflow-delete-broken-dags.py
@@ -15,12 +15,18 @@ import logging
 import airflow
 
 
-DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")  # airflow-delete-broken-dags
+# airflow-delete-broken-dags
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 START_DATE = airflow.utils.dates.days_ago(1)
-SCHEDULE_INTERVAL = "@daily"        # How often to Run. @daily - Once a day at Midnight
-DAG_OWNER_NAME = "operations"       # Who is listed as the owner of this DAG in the Airflow Web Server
-ALERT_EMAIL_ADDRESSES = []          # List of email address to send email alerts to if this job fails
-ENABLE_DELETE = True                # Whether the job should delete the logs or not. Included if you want to temporarily avoid deleting the logs
+# How often to Run. @daily - Once a day at Midnight
+SCHEDULE_INTERVAL = "@daily"
+# Who is listed as the owner of this DAG in the Airflow Web Server
+DAG_OWNER_NAME = "operations"
+# List of email address to send email alerts to if this job fails
+ALERT_EMAIL_ADDRESSES = []
+# Whether the job should delete the logs or not. Included if you want to
+# temporarily avoid deleting the logs
+ENABLE_DELETE = True
 
 default_args = {
     'owner': DAG_OWNER_NAME,
@@ -32,7 +38,12 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(DAG_ID, default_args=default_args, schedule_interval=SCHEDULE_INTERVAL, start_date=START_DATE)
+dag = DAG(
+    DAG_ID,
+    default_args=default_args,
+    schedule_interval=SCHEDULE_INTERVAL,
+    start_date=START_DATE
+)
 if hasattr(dag, 'doc_md'):
     dag.doc_md = __doc__
 if hasattr(dag, 'catchup'):
@@ -60,10 +71,14 @@ def delete_broken_dag_files(**context):
 
     errors = session.query(ImportError).all()
 
-    logging.info("Process will be removing broken DAG file(s) from the file system:")
+    logging.info(
+        "Process will be removing broken DAG file(s) from the file system:"
+    )
     for error in errors:
         logging.info("\tFile: " + str(error.filename))
-    logging.info("Process will be Deleting " + str(len(errors)) + " DAG file(s)")
+    logging.info(
+        "Process will be Deleting " + str(len(errors)) + " DAG file(s)"
+    )
 
     if ENABLE_DELETE:
         logging.info("Performing Delete...")

commit 9d13aa4dfe51bf36dab234a9938a622e81c053a1

Fix Keep Last Logic
It appears commit a5fc73b introduced a bug by specifying a `keep_last_filters` of `[DagRun.external_trigger is False]` this Python expression evaluates to `False` which then causes the subquery filter clause here:
https://github.com/teamclairvoyant/airflow-maintenance-dags/blob/master/db-cleanup/airflow-db-cleanup.py#L295-L296

To basically execute `subquery = subquery.filter(False)` which translates into the SQL predicate `WHERE false = 1` for the subquery and making it impossible for the latest record to be selected and stopping it from being deleted. With this patch applied the predicate becomes `WHERE dag_run.external_trigger IS false` which I think is the intended action for this code.

db-cleanup/airflow-db-cleanup.py
@@ -64,7 +64,7 @@ DATABASE_OBJECTS = [
         "airflow_db_model": DagRun,
         "age_check_column": DagRun.execution_date,
         "keep_last": True,
-        "keep_last_filters": [DagRun.external_trigger is False],
+        "keep_last_filters": [DagRun.external_trigger.is_(False)],
         "keep_last_group_by": DagRun.dag_id
     },
     {