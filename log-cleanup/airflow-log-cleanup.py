1a54c87f65fe88b3910b10eff9305d7fbf4bf8e5
Prakshal Jain <prakshalj0512@gmail.com>
2020-12-03 11:41:24 -0700

    revert

--- log-cleanup/airflow-log-cleanup.py
+++ log-cleanup/airflow-log-cleanup.py
@@ -5,16 +5,14 @@ airflow trigger_dag --conf '[curly-braces]"maxLogAgeInDays":30[curly-braces]' ai
 --conf options:
     maxLogAgeInDays:<INT> - Optional
 """
-import logging
-import os
-import time
-from datetime import timedelta
-
-import airflow
-from airflow.configuration import conf
 from airflow.models import DAG, Variable
+from airflow.configuration import conf
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
+from datetime import timedelta
+import os
+import logging
+import airflow
 
 # airflow-log-cleanup
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
@@ -33,21 +31,16 @@ DEFAULT_MAX_LOG_AGE_IN_DAYS = Variable.get(
 )
 # Whether the job should delete the logs or not. Included if you want to
 # temporarily avoid deleting the logs
-ENABLE_DELETE = False
-
-try:
-    AIRFLOW_HOSTS = conf.get("scheduler_failover",
-                             "scheduler_nodes_in_cluster")
-except Exception as e:
-    logging.info("Airflow Scheduler Failover Not Present...")
-    AIRFLOW_HOSTS = "localhost"
-
-TEMP_LOG_CLEANUP_SCRIPT_PATH = "/tmp/airflow_log_cleanup.sh"
+ENABLE_DELETE = True
+# The number of worker nodes you have in Airflow. Will attempt to run this
+# process for however many workers there are so that each worker gets its
+# logs cleared.
+NUMBER_OF_WORKERS = 1
 DIRECTORIES_TO_DELETE = [BASE_LOG_FOLDER]
 ENABLE_DELETE_CHILD_LOG = Variable.get(
     "airflow_log_cleanup__enable_delete_child_log", "False"
 )
-
+LOG_CLEANUP_PROCESS_LOCK_FILE = "/tmp/airflow_log_cleanup_worker.lock"
 logging.info("ENABLE_DELETE_CHILD_LOG  " + ENABLE_DELETE_CHILD_LOG)
 
 if not BASE_LOG_FOLDER or BASE_LOG_FOLDER.strip() == "":
@@ -92,105 +85,139 @@ if hasattr(dag, 'doc_md'):
 if hasattr(dag, 'catchup'):
     dag.catchup = False
 
+start = DummyOperator(
+    task_id='start',
+    dag=dag)
+
 log_cleanup = """
+
 echo "Getting Configurations..."
+BASE_LOG_FOLDER="{{params.directory}}"
+WORKER_SLEEP_TIME="{{params.sleep_time}}"
 
-BASE_LOG_FOLDER=$1
-MAX_LOG_AGE_IN_DAYS=$2
-ENABLE_DELETE=$3
+sleep ${WORKER_SLEEP_TIME}s
 
+MAX_LOG_AGE_IN_DAYS="{{dag_run.conf.maxLogAgeInDays}}"
+if [ "${MAX_LOG_AGE_IN_DAYS}" == "" ]; then
+    echo "maxLogAgeInDays conf variable isn't included. Using Default '""" + str(DEFAULT_MAX_LOG_AGE_IN_DAYS) + """'."
+    MAX_LOG_AGE_IN_DAYS='""" + str(DEFAULT_MAX_LOG_AGE_IN_DAYS) + """'
+fi
+ENABLE_DELETE=""" + str("true" if ENABLE_DELETE else "false") + """
 echo "Finished Getting Configurations"
 echo ""
 
 echo "Configurations:"
-echo "BASE_LOG_FOLDER:      \'${BASE_LOG_FOLDER}\'"
-echo "MAX_LOG_AGE_IN_DAYS:  \'${MAX_LOG_AGE_IN_DAYS}\'"
-echo "ENABLE_DELETE:        \'${ENABLE_DELETE}\'"
+echo "BASE_LOG_FOLDER:      '${BASE_LOG_FOLDER}'"
+echo "MAX_LOG_AGE_IN_DAYS:  '${MAX_LOG_AGE_IN_DAYS}'"
+echo "ENABLE_DELETE:        '${ENABLE_DELETE}'"
 
 cleanup() {
     echo "Executing Find Statement: $1"
-    FILES_MARKED_FOR_DELETE=$(eval $1)
-    echo "Process will be Deleting the following files or directories:"
+    FILES_MARKED_FOR_DELETE=`eval $1`
+    echo "Process will be Deleting the following File(s)/Directory(s):"
     echo "${FILES_MARKED_FOR_DELETE}"
-    echo "Process will be Deleting $(echo "${FILES_MARKED_FOR_DELETE}" |
-        grep -v \'^$\' | wc -l) files or directories"
-
+    echo "Process will be Deleting `echo "${FILES_MARKED_FOR_DELETE}" | \
+    grep -v '^$' | wc -l` File(s)/Directory(s)"     \
+    # "grep -v '^$'" - removes empty lines.
+    # "wc -l" - Counts the number of lines
     echo ""
-    if [ "${ENABLE_DELETE}" == "true" ]; then
-        if [ "${FILES_MARKED_FOR_DELETE}" != "" ]; then
+    if [ "${ENABLE_DELETE}" == "true" ];
+    then
+        if [ "${FILES_MARKED_FOR_DELETE}" != "" ];
+        then
             echo "Executing Delete Statement: $2"
             eval $2
             DELETE_STMT_EXIT_CODE=$?
             if [ "${DELETE_STMT_EXIT_CODE}" != "0" ]; then
-                echo "Delete process failed with exit code  \'${DELETE_STMT_EXIT_CODE}\'"
+                echo "Delete process failed with exit code \
+                    '${DELETE_STMT_EXIT_CODE}'"
 
+                echo "Removing lock file..."
+                rm -f """ + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """
+                if [ "${REMOVE_LOCK_FILE_EXIT_CODE}" != "0" ]; then
+                    echo "Error removing the lock file. \
+                    Check file permissions.\
+                    To re-run the DAG, ensure that the lock file has been \
+                    deleted (""" + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """)."
+                    exit ${REMOVE_LOCK_FILE_EXIT_CODE}
+                fi
                 exit ${DELETE_STMT_EXIT_CODE}
             fi
         else
-            echo "WARN: No files or directories to Delete"
+            echo "WARN: No File(s)/Directory(s) to Delete"
         fi
     else
-        echo "WARN: You have opted to skip deleting the files or directories"
+        echo "WARN: You're opted to skip deleting the File(s)/Directory(s)!!!"
     fi
 }
 
-echo ""
-echo "Running Cleanup Process..."
 
-FIND_STATEMENT="find ${BASE_LOG_FOLDER}/*/* -type f -mtime +${MAX_LOG_AGE_IN_DAYS}"
-DELETE_STMT="${FIND_STATEMENT} -exec rm -f {} \;"
+if [ ! -f """ + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """ ]; then
 
-cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
-CLEANUP_EXIT_CODE=$?
+    echo "Lock file not found on this node! \
+    Creating it to prevent collisions..."
+    touch """ + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """
+    CREATE_LOCK_FILE_EXIT_CODE=$?
+    if [ "${CREATE_LOCK_FILE_EXIT_CODE}" != "0" ]; then
+        echo "Error creating the lock file. \
+        Check if the airflow user can create files under tmp directory. \
+        Exiting..."
+        exit ${CREATE_LOCK_FILE_EXIT_CODE}
+    fi
 
-FIND_STATEMENT="find ${BASE_LOG_FOLDER}/*/* -type d -empty"
-DELETE_STMT="${FIND_STATEMENT} -prune -exec rm -rf {} \;"
+    echo ""
+    echo "Running Cleanup Process..."
 
-cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
-CLEANUP_EXIT_CODE=$?
+    FIND_STATEMENT="find ${BASE_LOG_FOLDER}/*/* -type f -mtime \
+     +${MAX_LOG_AGE_IN_DAYS}"
+    DELETE_STMT="${FIND_STATEMENT} -exec rm -f {} \;"
 
-FIND_STATEMENT="find ${BASE_LOG_FOLDER}/* -type d -empty"
-DELETE_STMT="${FIND_STATEMENT} -prune -exec rm -rf {} \;"
+    cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
+    CLEANUP_EXIT_CODE=$?
 
-cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
-CLEANUP_EXIT_CODE=$?
+    FIND_STATEMENT="find ${BASE_LOG_FOLDER}/*/* -type d -empty"
+    DELETE_STMT="${FIND_STATEMENT} -prune -exec rm -rf {} \;"
 
-echo "Finished Running Cleanup Process"
+    cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
+    CLEANUP_EXIT_CODE=$?
+
+    FIND_STATEMENT="find ${BASE_LOG_FOLDER}/* -type d -empty"
+    DELETE_STMT="${FIND_STATEMENT} -prune -exec rm -rf {} \;"
+
+    cleanup "${FIND_STATEMENT}" "${DELETE_STMT}"
+    CLEANUP_EXIT_CODE=$?
+
+    echo "Finished Running Cleanup Process"
+
+    echo "Deleting lock file..."
+    rm -f """ + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """
+    REMOVE_LOCK_FILE_EXIT_CODE=$?
+    if [ "${REMOVE_LOCK_FILE_EXIT_CODE}" != "0" ]; then
+        echo "Error removing the lock file. Check file permissions. To re-run the DAG, ensure that the lock file has been deleted (""" + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """)."
+        exit ${REMOVE_LOCK_FILE_EXIT_CODE}
+    fi
+
+else
+    echo "Another task is already deleting logs on this worker node. \
+    Skipping it!"
+    echo "If you believe you're receiving this message in error, kindly check \
+    if """ + str(LOG_CLEANUP_PROCESS_LOCK_FILE) + """ exists and delete it."
+    exit 0
+fi
+
 """
 
-create_log_cleanup_script = BashOperator(
-    task_id=f'create_log_cleanup_script',
-    bash_command=f"""
-    echo '{log_cleanup}' > {TEMP_LOG_CLEANUP_SCRIPT_PATH}
-    chmod +x {TEMP_LOG_CLEANUP_SCRIPT_PATH}
-    current_host=$(echo $HOSTNAME)
-    echo "Current Host: $current_host"
-    hosts_string={AIRFLOW_HOSTS}
-    echo "All Scheduler Hosts: $hosts_string"
-    IFS=',' read -ra host_array <<< "$hosts_string"
-    for host in "${{host_array[@]}}"
-    do
-        if [ "$host" != "$current_host" ]; then
-            echo "Copying log_cleanup script to $host..."
-            scp {TEMP_LOG_CLEANUP_SCRIPT_PATH} $host:{TEMP_LOG_CLEANUP_SCRIPT_PATH}
-            echo "Making the script executable..."
-            ssh $host "chmod +x {TEMP_LOG_CLEANUP_SCRIPT_PATH}"
-        fi
-    done
-    """,
-    dag=dag)
+for log_cleanup_id in range(1, NUMBER_OF_WORKERS + 1):
 
-for host in AIRFLOW_HOSTS.split(","):
     for dir_id, directory in enumerate(DIRECTORIES_TO_DELETE):
-        LOG_CLEANUP_COMMAND = f'{TEMP_LOG_CLEANUP_SCRIPT_PATH} {directory} {DEFAULT_MAX_LOG_AGE_IN_DAYS} {str(ENABLE_DELETE).lower()}'
-        cleanup_task = BashOperator(
-            task_id=f'airflow_log_cleanup_{host}',
-            bash_command=f"""
-            echo "Executing cleanup script..."
-            ssh {host} "{LOG_CLEANUP_COMMAND}"
-            echo "Removing cleanup script..."
-            ssh {host} "rm {TEMP_LOG_CLEANUP_SCRIPT_PATH}"
-            """,
+
+        log_cleanup_op = BashOperator(
+            task_id='log_cleanup_worker_num_' +
+            str(log_cleanup_id) + '_dir_' + str(dir_id),
+            bash_command=log_cleanup,
+            params={
+                "directory": str(directory),
+                "sleep_time": int(log_cleanup_id)*3},
             dag=dag)
 
-        cleanup_task.set_upstream(create_log_cleanup_script)
+        log_cleanup_op.set_upstream(start)
